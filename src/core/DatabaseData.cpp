8170cbd55a93826d35b408f801ece1a9fe2422a2
Gijs Kwakkel <info@gijsweb.net>
2012-09-05 01:35:21 +0300

    Update src/core/DatabaseData.cpp

--- src/core/DatabaseData.cpp
+++ src/core/DatabaseData.cpp
@@ -22,6 +22,9 @@
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 //
 
+
+// implement http://www.sqlapi.com/ later
+
 #include "../include/core/DatabaseData.h"
 
 #include <boost/algorithm/string.hpp>
